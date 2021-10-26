3d3327fc05bb9360aeb45fc039a756613d517563
Nouraoqail <Nouraoqail.10@gmail.com>
2021-10-27 00:06:45 +0300

    React HW

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Tweets from './Component/Tweets';
+import React from 'react';
+import ReactDOM from 'react-dom';
+function App() {
+  
+    return <Tweets/>
+//this code below from previous lab just ignore it :)
+//    <>
+//   <button><a href="https://tuwaiq.edu.sa/">Click here </a></button>
+//        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//  </>
+//     ReactDOM.render(App,document.getElementById("div"));
+    
+
+    }
+  
+// setInterval(App, 1000);
+export default App;
