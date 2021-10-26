d24aa4315a41932cbed892118ecaa22605289a0d
aljoharah21 <aljoharah.ali.ruh@tuwaiq.edu.sa>
2021-10-26 13:01:55 +0300

    myhw

---
+++ footer.js
@@ -0,0 +1,40 @@
+
+function footer() {
+    return (
+        <div>
+            <Header>
+
+
+                const Imagees=[
+                {
+                    id:1,
+                name:'judy',
+                src:'https://img.pikbest.com/png-images/qiantu/cartoon-customer-service-png-free_2726146.png!c1024wm0/compress/true/progressive/true/format/webp/fw/1024',
+                descripition:' <ul>
+                    <li>ifyou look at what you have in life,you'll always have more .if you look at what you dont have in life,you'll never have enough.-operah winfery</li>
+                    <li>you will face many defeats in life, but never let yourself be defeated.-maya angelou</li>
+                </ul>'
+                ]
+},
+
+
+
+
+
+
+
+
+
+                <h1>user name:judy</h1>
+                <h2>tweets</h2>
+                <ul>
+                    <li>ifyou look at what you have in life,you'll always have more .if you look at what you dont have in life,you'll never have enough.-operah winfery</li>
+                    
+                </ul>
+
+            </Header>
+        </div>
+    );
+}
+
+export default footer;
