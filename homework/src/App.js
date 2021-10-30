f295e5a4448e9429792b210369c6777ebdb9ffad
S-MW <72529306+S-MW@users.noreply.github.com>
2021-10-30 22:09:23 +0300

    Enhance the solution vie pass the objectOfArry via Props

--- homework/src/App.js
+++ homework/src/App.js
@@ -4,9 +4,28 @@ import Tweets from '../src/component/Tweets'
 
 
 function App() {
+
+  let obj = [
+    {
+    name:"saad",
+    image:"https://icons-for-free.com/iconfiles/png/512/boy+guy+man+icon-1320166733913205010.png",
+    tweets:["first Tweets : Saad", "2's Tweets : Saad","3th Tweets : Saad"]
+    },
+    {
+    name:"fahad",
+    image:"https://icons-for-free.com/iconfiles/png/512/boy+guy+man+icon-1320166733913205010.png",
+    tweets:["first Tweets : fahad", "2's Tweets : fahad"]
+    },
+    {
+    name:"shahad",
+    image:"https://cdn-icons-png.flaticon.com/512/146/146025.png",
+    tweets:["first Tweets : shahad"]
+    },
+]
+
   return (
     <div className="App">
-      <Tweets />
+      <Tweets obj={obj}/>
     </div>
   );
 }
--- homework/src/component/Tweets.js
+++ homework/src/component/Tweets.js
@@ -1,24 +1,7 @@
 import './Tweets.css';
 
-    let obj = [
-        {
-        name:"saad",
-        image:"https://icons-for-free.com/iconfiles/png/512/boy+guy+man+icon-1320166733913205010.png",
-        tweets:["first Tweets : Saad", "2's Tweets : Saad","3th Tweets : Saad"]
-        },
-        {
-        name:"fahad",
-        image:"https://icons-for-free.com/iconfiles/png/512/boy+guy+man+icon-1320166733913205010.png",
-        tweets:["first Tweets : fahad", "2's Tweets : fahad"]
-        },
-        {
-        name:"shahad",
-        image:"https://cdn-icons-png.flaticon.com/512/146/146025.png",
-        tweets:["first Tweets : shahad"]
-        },
-    ]
 
-    let showObj = obj.map((e,i)=>{
+    let showObj = (arrayOfObj)=> arrayOfObj.map((e,i)=>{
         return <div key={i} id="card">
                     <p>User Name : <b>{e.name}</b></p>
                     <img src={e.image} alt="Tweets Images" width="50" height="50"/>
@@ -30,10 +13,10 @@ import './Tweets.css';
             
     })
 
-    function Tweets() {
+    function Tweets({obj}) {
         return (
         <>
-            {showObj}
+            {showObj(obj)}
         </>
         );
     }
