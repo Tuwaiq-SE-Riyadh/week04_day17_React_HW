e2c2bd945e571ec83999efc2f9f7648e1a140455
AbdullahAlhussein <abdullah30alhussein@gmail.com>
2021-11-14 09:17:49 +0300

    update solution

--- newapp/src/Component/Tweets.js
+++ newapp/src/Component/Tweets.js
@@ -4,19 +4,19 @@ const TweetsList = [
     {
         name: "Abdullah",
         image: "https://dp-client.com/CMS-NEW/assets/images/user/user11605616227.png",
-        tweets: "Twitter is an American microblogging and social networking service on which users post and interact with messages known as tweets"
+        tweets: ["tweet 1 ", "tweet 2 ", "tweet 3 "]
     },
     {
 
         name: "Amal",
         image: "https://cdn.iconscout.com/icon/free/png-256/hijab-woman-1720800-1467190.png",
-        tweets: "Twitter is an American microblogging and social networking service on which users post and interact with messages known as tweets"
+        tweets: ["tweet 1 ", "tweet 2 ", "tweet 3 "]
     },
     {
 
         name: "Ali",
         image: "https://dp-client.com/CMS-NEW/assets/images/user/user11605616227.png",
-        tweets: "Twitter is an American microblogging and social networking service on which users post and interact with messages known as tweets"
+        tweets: ["tweet 1 ", "tweet 2 ", "tweet 3 "]
     },
 ]
 
@@ -31,7 +31,9 @@ function Tweets (){
               <h3>User Name: {post.name}</h3>
               <img src = {post.image} alt="user image"/>
               <h3>Tweets: </h3>
-              <ul><li>{post.tweets}</li></ul> 
+              <ul> {post.tweets.map((ele , ind)=>{
+                        return <li key={ind}>{ele}</li>
+                    })}</ul> 
               <hr/>
               </div> )}
 
