969f4874a50effd6ae5aad8c5730614f9f145d3d
Nouraoqail <Nouraoqail.10@gmail.com>
2021-11-15 10:04:14 +0300

    React update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,14 @@
 
 import './App.css';
-import Tweets from './Component/Tweets';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Tweets from "./Component/Tweets";
 function App() {
   
-    return <Tweets/>
-//this code below from previous lab just ignore it :)
-//    <>
-//   <button><a href="https://tuwaiq.edu.sa/">Click here </a></button>
-//        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//  </>
-//     ReactDOM.render(App,document.getElementById("div"));
-    
 
-    }
-  
-// setInterval(App, 1000);
+  return (
+    <div>
+      <Tweets />
+    </div>
+  );
+  }
 export default App;
